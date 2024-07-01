38a3e5f092bcf05d192bbb18364a703b2d6914eb
Angelo Resplandes <angeloodrigues@gmail.com>
2024-07-01 15:04:39 -0300

    add design responsive on project

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  window.onload = function () {
+    document.querySelector(".menu-opener").addEventListener("click", function () {
+      if (document.querySelector("nav").style.display == 'flex') {
+        document.querySelector("nav").style.display = 'none';
+      } else {
+        document.querySelector("nav").style.display = 'flex';
+      }
+    });
+  };
+});
